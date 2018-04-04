e41199ead8fa613139de7d6a186c7db81a259e2b
Lucas Alves <lucas.alves.costa96@gmail.com>
2018-04-03 22:15:39 -0300

    Removed Comment component

--- frontend/src/components/PostDetail/index.js
+++ frontend/src/components/PostDetail/index.js
@@ -1,76 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchPost, addCommentAction } from '../../actions'
-import uuidv1 from 'uuid/v1'
-import PostInfo from './PostInfo'
-import { CommentForm } from './CommentForm'
-import CommentList from './CommentList'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost } from "../../actions";
 
+import PostInfo from "./PostInfo";
+import NotFound from "./NotFound";
 
 class PostDetail extends Component {
-
-  state = {
-    txtComment: ''
-  }
-
   componentDidMount() {
-    const { id } = this.props.match.params
-    this.props.getPost(id)
-  }
-
-  onInputChange = (e) => {
-    this.setState({
-      txtComment: e.target.value
-    })
-  }
-
-  onCommentSubmit = (e) => {
-    e.preventDefault();
-    if (this.state.txtComment) {
-      const newComment = {
-        id: uuidv1(),
-        timestamp: Date.now(),
-        body: this.state.txtComment,
-        author: this.props.post.post.author,
-        parentId: this.props.post.post.id
-      }
-      // POST new comment
-      this.props.addComment(newComment)
-        .then(() => {
-          this.setState({
-            txtComment: ''
-          })
-        })
-    }
+    const { id } = this.props.match.params;
+    this.props.getPost(id);
   }
 
   render() {
-    const { post } = this.props.post
-    const { comments } = this.props.post.post
-    return(
+    const { post } = this.props.post;
+
+    return (
       <div className="PostDetail">
         <PostInfo post={post} />
-        <CommentForm
-          txtComment={this.state.txtComment}
-          onCommentSubmit={this.onCommentSubmit}
-          onInputChange={this.onInputChange} />
-        <CommentList comments={comments} />
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = ({ post }) => {
   return {
     post: post
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    getPost: (id) => dispatch(fetchPost(id)),
-    addComment: (comment) => dispatch(addCommentAction(comment))
-  }
-}
+    getPost: id => dispatch(fetchPost(id))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostDetail)
+export default connect(mapStateToProps, mapDispatchToProps)(PostDetail);
